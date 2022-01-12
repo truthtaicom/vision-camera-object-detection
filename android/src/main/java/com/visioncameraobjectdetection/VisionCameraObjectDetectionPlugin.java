dd38c4cffdf4e1736dc2a0505ee0fb50638fdb1c
willcray <will.c.cray@gmail.com>
2022-01-12 10:38:47 -0600

    Objects list is empty

--- android/src/main/java/com/visioncameraobjectdetection/VisionCameraObjectDetectionPlugin.java
+++ android/src/main/java/com/visioncameraobjectdetection/VisionCameraObjectDetectionPlugin.java
@@ -1,7 +1,6 @@
 package com.visioncameraobjectdetection;
 
 import android.annotation.SuppressLint;
-import android.graphics.Rect;
 import android.media.Image;
 import android.util.Log;
 
@@ -12,6 +11,10 @@ import com.facebook.react.bridge.WritableNativeMap;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
 import com.google.mlkit.vision.objects.DetectedObject;
 import com.google.mlkit.vision.objects.ObjectDetection;
 import com.google.mlkit.vision.objects.ObjectDetector;
@@ -24,14 +27,8 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class VisionCameraObjectDetectionPlugin extends FrameProcessorPlugin {
-  // Live detection and tracking
-  ObjectDetectorOptions options =
-    new ObjectDetectorOptions.Builder()
-      .setDetectorMode(ObjectDetectorOptions.STREAM_MODE)
-//      .enableClassification()  // Optional
-      .build();
 
-  ObjectDetector objectDetector = ObjectDetection.getClient(options);
+  private final ObjectDetector objectDetector = ObjectDetection.getClient(ObjectDetectorOptions.DEFAULT_OPTIONS);
 
   @Override
   public Object callback(ImageProxy frame, @NotNull Object[] params) {
@@ -43,27 +40,14 @@ public class VisionCameraObjectDetectionPlugin extends FrameProcessorPlugin {
 
       try {
         List<DetectedObject> objects = Tasks.await(task);
+        Log.d("XXX", String.valueOf(objects.isEmpty()));
+
 
         WritableNativeArray array = new WritableNativeArray();
-        Log.d("TAG", String.valueOf(objects.size()));
 
-        for (DetectedObject object : objects) {
-          Rect boundingBox = object.getBoundingBox();
-          Integer trackingId = object.getTrackingId();
-          WritableNativeMap map = new WritableNativeMap();
-          map.putInt("left", boundingBox.left);
-          map.putInt("top", boundingBox.top);
-          map.putInt("right", boundingBox.right);
-          map.putInt("bottom", boundingBox.bottom);
-          map.putInt("trackingID", trackingId);
-
-          Log.d("TAG", " boundingBox: (${box.left}, ${box.top}) - (${box.right},${box.bottom})");
-          Log.d("TAG", " trackingId: ${trackingId}");
-
-          array.pushMap(map);
-        }
         return array;
       } catch (Exception e) {
+        Log.d("XXX", "ERROR");
         e.printStackTrace();
       }
     }
